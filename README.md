b9db3c7c4578016015308fe19f7eddd4a7b5fc93
Justin Stahlman <jstahlman@mac.com>
2016-07-02 11:33:59 -0400

    refactor working demo; new readme image and instructions;

--- README.md
+++ README.md
@@ -1,11 +1,15 @@
-# upload image files with Multer.js
+# Upload image files Node, Express and Multer.js
 
 This example allows for uploading a file by:
-- selecting on your machine
+- selecting a file on your machine
 - entering a URL
+- saving to an uploads folder on your server
+- renaming the file with a datestamp
+- limits file size to 1.5 MB (can be changed)
 
 ##TODO
 - validation of non-conform files
+	- currently allows uploading any kind of file
 
 ## Start
 Type these commands in the terminal inside the folder where you cloned the repository
@@ -13,5 +17,8 @@ Type these commands in the terminal inside the folder where you cloned the repos
 - `nodemon` if not installed `npm install nodemon -g`
 - `open http://localhost:3000/`
 
+## Resources
+- https://github.com/expressjs/multer
+
 ---
-<img src='preview.png'/>
+<img src='example.gif'/>
--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 var express = require('express');
 var fs = require('fs');
 var request = require('request');
-
 var url = require('url'); // to parse URL and separate filename from path
+
+var app = express();
 
 var multer = require('multer'); // library to uplaod photos https://github.com/expressjs/multer
 
@@ -13,11 +14,11 @@ var storage = multer.diskStorage({
     },
     filename: function (req, file, cb) {
         //console.log(file.mimetype)
-        cb(null, file.originalname + '-' + Date.now() + '-' +  getExtension(file));
+        cb(null, file.originalname + '-' + Date.now() + '-' + getExtension(file));
     }
 });
 
-function getExtension(file){
+function getExtension(file) {
     // this function gets the filename extension by determining mimetype. To be exanded to support others, for example .jpeg or .tiff
     var res = '';
     if (file.mimetype === 'image/jpeg') res = '.jpg';
@@ -25,65 +26,65 @@ function getExtension(file){
     return res;
 }
 
+app.use(express.static('./')); // serve all files in root folder, such as index.html
+
 // initialize Multer with storage definition and other options like limit to file size that can be uploaded
 var upload = multer({
     storage: storage,
-    limits: { fileSize: 100000, files: 1 },
-    onFileUploadStart: function (file) {
-        console.log('Upload starting for filename: ' + file.originalname);
-    },
-    onFileUploadData: function (file, data) {
-        console.log(data.length + ' of ' + file.fieldname + ' arrived')
-    },
-    onFileUploadComplete: function (file) {
-      console.log(file.fieldname + ' uploaded to  ' + file.path)
-    }
-    //,fileFilter: // another possible option
-});
-
-var app = express()
-
-app.use(express.static('./')); // serve all files in root folder, such as index.html
-
-var cpUpload = upload.fields([  // fields to accept multiple types of uploads
-  { name: "file", maxCount: 1 }, // in <input name='file' />
-  { name: "imageUrl", maxCount: 1 } // in <input name='imageUrl' />
-])
+    limits: { fileSize: 1048576, files: 1 } // limit file size to 1048576 bytes or 1 MB
+    //,fileFilter: // TODO limit types of files. currently can upload a .txt or any kind of file into uploads folder
+}).fields([ // fields to accept multiple types of uploads
+    { name: "fileName", maxCount: 1 } // in <input name='file' />
+]);
 
 // for input type=file
-app.post('/uploads', cpUpload, function (req, res, next) {
-    //console.log(req.files); // the file 'name' from input type=file
-    //console.log(req.body); // the text fields, if there were any
+app.post('/uploads', function (req, res, next) {
+    upload(req, res, function (err) {
+        if (err) {
+            res.status(err.status || 500).json({ "error": { "status_code": err.status || 500, "message": err.code } });
+            return;
+        } else {
 
-    //res.status(200).json({"success": { "status_code": 200, "message": "File successfully uploaded." }})
-    //.end("<h1>uploads</h2><a href='/'>Go back</a>")
+          // NOTE on what you can expect here
+          // console.log(req.file); // if using upload.single('yourInputName')
+          // console.log(req.files); // if using upload.fields([]); // array of input field names
+          // console.log(req.body); // if using a text field instead of file input, ex. to grab url from another site by path name
 
-    res.end("<h1>Uploaded</h2><a href='/'>Go back</a>")
+            if (req.files.fileName) { // fileName comes from input element:   <input type="file" name="fileName">
+                var reqJSON = JSON.stringify(req.files.fileName, null, 2); // pretty print the JSON for <pre> tag
+                res.end("<h1>Uploaded from file</h2><img style='max-width:20%' src='" + req.files.fileName[0].path + "'/><pre>" + reqJSON + "</pre><a href='/'>Go back</a>")
+                //console.log("req.files.fileName")
+                //console.log(req.files.fileName)
+            }
+            else if (req.body.imageUrl) {
 
-    // here we grab the text entered into the optional text field for a remote url image
-    // @params (text string entered into input field, filename to save)
+              // the text field was used, so process the input type=text with regular node/express
+                var download = function (uri, filename, callback) {
+                    request.head(uri, function (err, res, body) {
+                        console.log('content-type:', res.headers['content-type']);
+                        console.log('content-length:', res.headers['content-length']);
+                        request(uri).pipe(fs.createWriteStream('./uploads/' + filename)).on('close', callback);
+                    });
+                };
 
-
-
-    // now process the input type=text with regular node/express
-    var download = function (uri, filename, callback) {
-        request.head(uri, function (err, res, body) {
-            console.log('content-type:', res.headers['content-type']);
-            console.log('content-length:', res.headers['content-length']);
-            request(uri).pipe(fs.createWriteStream('./uploads/' + filename)).on('close', callback);
-        });
-    };
-
-    // this is only available when submitting a text url, not by choosing file to upload
-    var urlParsed = url.parse(req.body.imageUrl);
-    if (urlParsed.pathname){
-      var onlyTheFilename = urlParsed.pathname ? urlParsed.pathname.substring(urlParsed.pathname.lastIndexOf('/') + 1).replace(/((\?|#).*)?$/, '') : '';
-      //console.log(urlParsed)
-      download(urlParsed.href, onlyTheFilename + '-' + Date.now() + '-' + onlyTheFilename, function () {
-          console.log('done');
-      });
-    }
-})
+                // this is only available when submitting a text url, not by choosing file to upload
+                var urlParsed = url.parse(req.body.imageUrl);
+                if (urlParsed.pathname){
+                  var onlyTheFilename = urlParsed.pathname ? urlParsed.pathname.substring(urlParsed.pathname.lastIndexOf('/') + 1).replace(/((\?|#).*)?$/, '') : '';
+                  //console.log(urlParsed)
+                  var newFilename = onlyTheFilename + '-' + Date.now() + '-' + onlyTheFilename
+                  var wholePath = 'uploads/' + newFilename;
+                  download(urlParsed.href, newFilename, function () {
+                    var reqJSON = JSON.stringify(wholePath, null, 2); // pretty print
+                    res.end("<h1>Uploaded from URL</h2><img style='max-width:50%' src='" + wholePath + "'/><pre>" + reqJSON + "</pre><a href='/'>Go back</a>")
+                    console.log("wholePath")
+                    console.log(wholePath)
+                  });
+                }
+            }
+        }
+    });
+});
 
 app.listen(3000, function () {
     console.log("Working on port 3000");
